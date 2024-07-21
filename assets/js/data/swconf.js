4bb2545efa623d1892b8e83bb999b01c49476049
HeoMinSeok <HeoMinSeok@users.noreply.github.com>
2024-07-21 09:56:02 +0000

    deploy: c2f18c7b88eec74dc2d3ae51f8ea854c5700e6cd

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721546384',resources: [
+    cacheName: 'chirpy-1721555761',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
